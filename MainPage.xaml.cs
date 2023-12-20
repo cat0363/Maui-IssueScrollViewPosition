f02a99d33c20603dfbf11b4eff9a31038400862d
cat0363 <kenji.nagano.0363@gmail.com>
2023-12-20 13:54:43 +0900

    Add new repro code

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Maui_IssueScrollViewPosition
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void svTest_Scrolled(object sender, ScrolledEventArgs e)
+        {
+            txtPosition.Text += ("(x, y) = (" + e.ScrollX + ", " + e.ScrollY + ")" + Environment.NewLine);
+        }
+    }
+
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui_IssueScrollViewPosition
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
